20fe44affc051e7448be4251046b367a178ddb67
luizzrosario <luizzrosario@hotmail.com>
2025-02-03 10:21:04 -0300

    projeto funcional

--- semaforo.c
+++ semaforo.c
@@ -1,66 +1,55 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/pio.h"
-#include "hardware/uart.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
 
-#include "blink.pio.h"
+#define VERMELHO 11
+#define AMARELO 12
+#define VERDE 13
 
-void blink_pin_forever(PIO pio, uint sm, uint offset, uint pin, uint freq) {
-    blink_program_init(pio, sm, offset, pin);
-    pio_sm_set_enabled(pio, sm, true);
+int estado = 0; // Estado do semáforo
 
-    printf("Blinking pin %d at %d Hz\n", pin, freq);
-
-    // PIO counter program takes 3 more cycles in total than we pass as
-    // input (wait for n + 1; mov; jmp)
-    pio->txf[sm] = (125000000 / (2 * freq)) - 3;
+// Função para configurar os pinos dos LEDs
+void configurar_pinos() {
+    gpio_init(VERMELHO);
+    gpio_set_dir(VERMELHO, GPIO_OUT);
+    gpio_init(AMARELO);
+    gpio_set_dir(AMARELO, GPIO_OUT);
+    gpio_init(VERDE);
+    gpio_set_dir(VERDE, GPIO_OUT);
 }
 
-// UART defines
-// By default the stdout UART is `uart0`, so we will use the second one
-#define UART_ID uart1
-#define BAUD_RATE 115200
+// Função chamada pelo timer a cada 3 segundos
+bool mudar_semaforo(repeating_timer_t *t) {
+    // Apaga todos os LEDs
+    gpio_put(VERMELHO, 0);
+    gpio_put(AMARELO, 0);
+    gpio_put(VERDE, 0);
 
-// Use pins 4 and 5 for UART1
-// Pins can be changed, see the GPIO function select table in the datasheet for information on GPIO assignments
-#define UART_TX_PIN 4
-#define UART_RX_PIN 5
-
-
-
-int main()
-{
-    stdio_init_all();
-
-    // PIO Blinking example
-    PIO pio = pio0;
-    uint offset = pio_add_program(pio, &blink_program);
-    printf("Loaded program at %d\n", offset);
-    
-    #ifdef PICO_DEFAULT_LED_PIN
-    blink_pin_forever(pio, 0, offset, PICO_DEFAULT_LED_PIN, 3);
-    #else
-    blink_pin_forever(pio, 0, offset, 6, 3);
-    #endif
-    // For more pio examples see https://github.com/raspberrypi/pico-examples/tree/master/pio
-
-    // Set up our UART
-    uart_init(UART_ID, BAUD_RATE);
-    // Set the TX and RX pins by using the function select on the GPIO
-    // Set datasheet for more information on function select
-    gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
-    gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-    
-    // Use some the various UART functions to send out data
-    // In a default system, printf will also output via the default UART
-    
-    // Send out a string, with CR/LF conversions
-    uart_puts(UART_ID, " Hello, UART!\n");
-    
-    // For more examples of UART use see https://github.com/raspberrypi/pico-examples/tree/master/uart
-
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    // Acende o LED correspondente
+    if (estado == 0) {
+        gpio_put(VERMELHO, 1);
+        estado = 1;
+    } else if (estado == 1) {
+        gpio_put(AMARELO, 1);
+        estado = 2;
+    } else {
+        gpio_put(VERDE, 1);
+        estado = 0;
     }
+    return true; // Mantém o timer funcionando
+}
+
+int main() {
+    stdio_init_all();
+    configurar_pinos(); // Configura os pinos dos LEDs
+
+    repeating_timer_t timer;
+    add_repeating_timer_ms(3000, mudar_semaforo, NULL, &timer);
+
+    while (1) {
+        tight_loop_contents(); // Mantém o programa rodando
+    }
+
+    return 0;
 }
